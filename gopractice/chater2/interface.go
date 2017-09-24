42cc4e08fde789acfe28c14bc570c931fb833513
gehaiyang <ghyxq1010@163.com>
2017-09-25 00:00:24 +0800

    defer使用方式

--- gopractice/chater2/interface.go
+++ gopractice/chater2/interface.go
@@ -2,16 +2,18 @@ package main
 
 import "fmt"
 
+//_,ok := talk.(&Mytalk) 这句话还有问题
 
+
+//接口定义
 type talk interface {
 	Hello()
 	World()
 }
 
 
-
+//实现上面的接口，下面33行用了类型断言
 type Mytalk string  //mytalk实现了talk类型
-
 
 func (m *Mytalk) Hello()  {
 
@@ -28,6 +30,7 @@ func main()  {
 	s := Mytalk("ok")
 	var talktest talk= &s    //这里不能直接写var talktest talk = &Mytalk("ok") 会报找不到 cannot take the address of Mytalk("hello")
 
+	//类型断言，判断talktest是否实现了talk接口
 	ok := talktest.(talk)
 	fmt.Println("hello world",ok)
 
